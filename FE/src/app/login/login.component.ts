9daa2f109fb979b3e20717d466cc4418b34e075e
SimonaAntal <simona.antal@student.tuiasi.ro>
2025-05-18 13:22:52 +0300

    Login Page password check

--- FE/src/app/login/login.component.ts
+++ FE/src/app/login/login.component.ts
@@ -1,12 +1,12 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import {FormsModule, NgForm} from '@angular/forms';
 import {User} from './user.model';
-import {Router} from '@angular/router';
+import {Router} from "@angular/router";
+import {NgIf} from "@angular/common";
 
 @Component({
     selector: 'app-login',
-    standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, NgIf],
     templateUrl: "login.component.html",
     styleUrls: ["login.component.css"]
 })
@@ -17,10 +17,12 @@ export class LoginComponent implements OnInit {
     @ViewChild('loginButton') loginButtonRef?: ElementRef;
     @ViewChild('signUpButton') signUpButtonRef?: ElementRef;
 
-  constructor(
-    private router: Router) {
+    constructor(
+        private router: Router) {
 
-  }
+    }
+
+    passwordError: string = "";
 
     ngOnInit() {
     }
@@ -35,14 +37,50 @@ export class LoginComponent implements OnInit {
     onLogin(form: NgForm) {
         if (form.valid) {
             console.log('Login form submitted', this.user)
-          this.router.navigate(['/home']);
+            this.router.navigate(['/home']);
         }
     }
 
     onSignUp(form: NgForm) {
         if (form.valid) {
+            //ruta la home menu component
+            //this.route
             console.log('Signup form submitted', this.user);
-          this.router.navigate(['/home']);
+            this.router.navigate(['/home']);
         }
+    }
+
+    verifyPassword() {
+        console.log("login")
+        if (this.user.password == undefined) {
+            this.passwordError = "Password cannot be null";
+            return;
+        }
+
+        if (this.user.password.length < 8) {
+            this.passwordError = "Password must have at least 8 characters";
+        }
+
+        if (!/[A-Z]/.test(this.user.password)) {
+            this.passwordError = "Password must contain at least one uppercase";
+            return;
+        }
+
+        if (!/[a-z]/.test(this.user.password)) {
+            this.passwordError = "Password must contain at least one lowercase";
+            return;
+        }
+
+        if (!/[0-9]/.test(this.user.password)) {
+            this.passwordError = "Password must contain at least one digit";
+            return;
+        }
+
+        if (!/[!@#$%^&*(),.?":{}|<>]/.test(this.user.password)) {
+            this.passwordError = "Password must contain at least one special character";
+            return;
+        }
+
+        this.passwordError = "";
     }
 }
