a5ab116ce0d3e099cb71dba12904d8f0ca3b647b
GherMa <madalinghergut@gmail.com>
2025-05-18 11:46:13 +0300

    Conversation Start

---
+++ BE/src/main/java/controller/ConversationController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import entity.Conversation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.ConversationService;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/conversation-service")
+public class ConversationController {
+
+    private ConversationService conversationService;
+
+    @GetMapping("")
+    private List<Conversation> getAllConversations() {
+        return null;
+    }
+
+
+}
---
+++ BE/src/main/java/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package dto;
+
+import enums.ROLES;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+public class UserDTO {
+    private UUID id;
+    private String name;
+    private String email;
+    private ROLES role;
+    private Integer coins;
+    private Boolean premium;
+
+    public UserDTO(UUID id, String name, String email, ROLES role, Integer coins, Boolean premium) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.coins = coins;
+        this.premium = premium;
+    }
+}
